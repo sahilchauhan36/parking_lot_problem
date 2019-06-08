f9b25e3fbf5bdb49a5978d70daf533565e955b7a
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 02:24:36 +0530

    Added Various Basic Enums
    Added Enums to keep for handing Messages separately and maintaining consistency.

---
+++ library/notification_enums.py
@@ -0,0 +1,10 @@
+# File containing various message
+
+
+class notification_message:
+    FULL_PARKING = "Sorry, parking lot is full"
+    INVALID_PARKING_LOT = "Sorry, Invalid Parking Lot with zero space"
+    INVALID_SLOT_NUMBER = "Invalid Slot Number"
+    NO_CAR_FOUND = "Sorry, No Car Found"
+    EMPTY_PARKING_LOT = "Parking Lot is Empty"
+    NOT_FOUND = "Not Found"
