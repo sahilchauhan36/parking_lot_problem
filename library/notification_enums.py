73683ade2c3906794489de84c7daeddf48c83512
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 14:25:32 +0530

    Updated Documentation and Class Name

--- library/notification_enums.py
+++ library/notification_enums.py
@@ -1,10 +1,16 @@
 # File containing various message
 
 
-class notification_message:
+class NotificationMessage:
+    """
+    Various Notification Message to be displayed to user.
+    """
     FULL_PARKING = "Sorry, parking lot is full"
     INVALID_PARKING_LOT = "Sorry, Invalid Parking Lot with zero space"
     INVALID_SLOT_NUMBER = "Invalid Slot Number"
     NO_CAR_FOUND = "Sorry, No Car Found"
     EMPTY_PARKING_LOT = "Parking Lot is Empty"
-    NOT_FOUND = "Not Found"
+    NOT_FOUND = "Not found"
+    NO_PARKING_LOT = "No Parking Lot"
+
+
