241f4d36d1e6c7bebea1b81ff22185ccec5b1cbe
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 16:08:32 +0530

    Added Tests for various P0 Parking Scenarios

---
+++ tests/functional_test/parking_test.py
@@ -0,0 +1,51 @@
+class ParkingTest:
+    def setup(self):
+        """
+        Setup Method to put comman things
+
+        - Create a Parking lot with valid size.
+        """
+    def test_park_car_without_color(self):
+        """
+        Test to verify parking of Car with no color specified.
+
+        Test Steps:
+        1. Park a Car without color specification.
+        2. Verify Assertion Message.
+        """
+
+    def test_park_car_without_regn_number(self):
+        """
+        Test to verify parking of Car with no registration number specified.
+
+        Test Steps:
+        1. Park a Car without registration number specification.
+        2. Verify Assertion Message.
+        """
+
+    def test_park_car_without_anything(self):
+        """
+        Test to verify parking of Car with no information specified.
+
+        Test Steps:
+        1. Park a Car without any specification.
+        2. Verify Assertion Message.
+        """
+
+    def test_park_car_with_special_char_in_reg_no(self):
+        """
+        Test to park a car having special characters in the Registration Num.
+
+        Test Steps:
+        1. Park a Car containing special character in registration no.
+        2. Verify Assertion Message.
+        """
+
+    def test_park_car_with_space_as_input(self):
+        """
+        Test to park a car with just space as input.
+
+        Test Steps:
+        1. Park a Car with Space as Name.
+        2. Park a Car with Space as Registration Number.
+        """
