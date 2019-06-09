f1977012ebe173e19506e8a4c862f45344e462c0
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 16:14:23 +0530

    Added Unit test for P0 functionality

---
+++ tests/unit_test/basic_acceptance_test.py
@@ -0,0 +1,12 @@
+class BasicAcceptanceTests:
+    def test_parking_lot_intialization:
+        
+    def test_park:
+    
+    def test_leave:
+        
+    def test_get_slot_number_from_car_color:
+    
+    def test_status:
+    
+    def test_get_reg_no_from_color:
