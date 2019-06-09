ab6cfdec8d6c7a4d0a26cde0598550fafc5812ef
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 14:24:27 +0530

    Created various Enums for Messages to display

---
+++ library/command_enums.py
@@ -0,0 +1,11 @@
+# File Containing various Commands Enums
+
+class CommandEnums:
+    CREATE_PARKING = "create_parking_lot"
+    PARK = "park"
+    LEAVE = "leave"
+    STATUS = "status"
+    GET_SLOT_FROM_COLOR = "slot_numbers_for_cars_with_colour"
+    GET_SLOT_FROM_REG_NO = "slot_number_for_registration_number"
+    GET_REGN_NO_FROM_COLOR = "registration_numbers_for_cars_with_colour"
+    EXIT = "exit"
