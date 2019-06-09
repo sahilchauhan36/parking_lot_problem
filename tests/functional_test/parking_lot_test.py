9daaaf62ba9551246898f5b18951a43f805bc9ac
Sahil Chauhan <sahilchauhan36@gmail.com>
2019-06-09 16:09:36 +0530

    Added Parking Lot based tests

---
+++ tests/functional_test/parking_lot_test.py
@@ -0,0 +1,28 @@
+
+class ParkingLotTests:
+    def test_parking_lot_of_size_zero(self):
+        """
+        Test to verify Leave on Invalid Slot.
+
+        Test Steps:
+        1. Create a Parking Lot with size 0.
+        2. Try to Park One Car and Verify the assertion event.
+        """
+
+    def test_parking_lot_of_size_negative(self):
+        """
+        Test to verify Leave on Invalid Slot.
+
+        Test Steps:
+        1. Create another Parking Lot with size -1.
+        2. Try to Park One Car and Verify the assertion event.
+        """
+
+    def test_parking_lot_without_size(self):
+        """
+        Test to Create a Parking Lot without specifying size.
+
+        Test Steps:
+        1. Try to Create a Parking Lot without specifying size.
+        2. Verify the assertion message.
+        """
